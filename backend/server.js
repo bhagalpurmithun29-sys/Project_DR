74ec7e7e78d1a4ee48969e62f25ffd1b0e5c4744
bhagalpurmithun29-sys <bhagalpur.mithun.29@gmail.com>
2026-05-04 14:20:32 +0530

    error fix

--- backend/server.js
+++ backend/server.js
@@ -97,7 +97,7 @@ app.use('/api', (req, res) => {
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../frontend/dist')));
-    app.get('/*', (req, res) => {
+    app.get('/:any*', (req, res) => {
         res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html'));
     });
 } else {
